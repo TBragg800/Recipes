301400ff8997624f6fd26692c81786ae03929baf
LM&T <tbbragg83@gmail.com>
2021-11-10 02:33:25 -0600

    assessment completed

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ textareas = document.querySelectorAll("textarea");
 recipeDiv = document.querySelector(".recipe-div");
 specialsDiv = document.querySelector(".specials-div");
 
-recipesBtn.addEventListener("click", (e) => {getRecipes();showDiv(e)});
+recipesBtn.addEventListener("click", (e) => {showRecipes();showDiv(e)});
 specialsBtn.addEventListener("click", (e) => {getSpecials();showDiv(e);});
 postBtn.addEventListener("click", (e) => {showDiv(e);getValues();});
 inputs.forEach(input => input.addEventListener("keydown", getValues));
 textareas.forEach(input => input.addEventListener("keydown", getValues));
 submitBtn.addEventListener('click', newRecipe);
+
+$(document).ready(function () {
+  console.log("Thank you for giving me an opportunity to be in front of you. Your consideration means a lot and I'm exicted to hear learn from you. \nBest, \nThomas Bragg");
+});
 
 function showDiv(e) {
   $(".containers").addClass("animOffScreen");
@@ -25,56 +29,57 @@ function showDiv(e) {
         : $(".new-recipes-container").removeClass("d-none animOffScreen") }, 500);
 }
 
-// function getRecipes() {
-//   fetch("http://localhost:3001/recipes")
-//     .then(function (response) {
-//       return recipes = response.json();
-//     })
-//     .then(fetch("http://localhost:3001/specials")
-//       .then(function (response) {
-//         return specials = response.json();
-//       })
-//       .then(function (specials) {
-//         console.log(specials)
-//         return specials;
-//       }))
-//     .then(function (recipes, specials) {
-//       console.log(specials)
-//       recipeDiv.innerHTML = recipes.map(function (recipe) {
-//         if (recipe.images) {
-//           image = recipe.images.small
-//         } else {
-//           image = "/img/pancake_mountain--s.jpg";
-//         }
-//         let ingredientContent = recipe.ingredients.map(function (ingredient) {
-//           return `<li class="list-group-item">${ingredient.amount} ${ingredient.measurement} - ${ingredient.name}</li>`
-//         }).join("\n")
-        
-//         let directionContent = recipe.directions.map(function (direction) {
-//             return `<li class="list-group-item">${direction.instructions}</li>`
-//         }).join("\n")
-  
-//         return `
-//                 <div class="card-header recipeCardHeader recipe" data="${recipe.title}">
-//                     <h2 class="card-title">${recipe.title}</h2>
-//                     <img class="card-title" src="http://localhost:3001/${image}" >
-//                     <h2 class="card-title">Servings: ${recipe.servings}</h2>
-//                 </div>
-//                 <div class="card-body">
-//                     <ul class="recipe-ul">Ingredients:
-//                         ${ingredientContent}
-//                     </ul>
-//                     <ulclass="recipe-ul">Directions:
-//                         ${directionContent}
-//                     </ul>
-//                     <ul class="recipe-ul text-center">Prep Time:${recipe.prepTime}&nbsp&nbsp&nbsp&nbspCook Time:${recipe.cookTime}</ul>
-//                 </div>`
-//       })
-//     })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     })
-// };
+function showRecipes() {
+  Promise.all([
+    fetch('http://localhost:3001/recipes'),
+    fetch('http://localhost:3001/specials')
+  ]).then(function (responses) {
+    return Promise.all(responses.map(function (response) {
+      return response.json();
+    }));
+  }).then(function (data) {
+    recipes = data[0];
+    specials = data[1];
+    let specialIngredientIds = specials.map((s) => { return s.ingredientId });
+    recipeDiv.innerHTML = recipes.map(function (recipe) {
+      if (recipe.images) {
+        image = recipe.images.small
+      } else {
+        image = "/img/pancake_mountain--s.jpg";
+      }
+      let ingredientContent = recipe.ingredients.map(function (ingredient) {
+        let ingredientLineItems = `<li class="list-group-item" data-key="${ingredient.uuid}">${ingredient.amount} ${ingredient.measurement} - ${ingredient.name}</li>`
+        if (specialIngredientIds.indexOf(ingredient.uuid) > -1) {
+          let special = specials.find(s => { return s.ingredientId === ingredient.uuid })
+          ingredientLineItems += `<li class="list-group-item">Special: *<span>${special.title} - ${special.type} - ${special.text}</span>*</li>`
+        }
+        return ingredientLineItems
+      }).join("\n")
+
+      let directionContent = recipe.directions.map(function (direction) {
+        return `<li class="list-group-item">${direction.instructions}</li>`
+      }).join("\n")
+
+      return `<div class="card-header recipeCardHeader recipe" data="${recipe.title}">
+                  <h2 class="card-title">${recipe.title}</h2>
+                  <img class="card-title" src="http://localhost:3001/${image}" >
+                  <h2 class="card-title">Servings: ${recipe.servings}</h2>
+              </div>
+              <div class="card-body">
+                  <ul class="recipe-ul">Ingredients:
+                      ${ingredientContent}
+                  </ul>
+                  <ulclass="recipe-ul">Directions:
+                      ${directionContent}
+                  </ul>
+                  <ul class="recipe-ul text-center"><span>Prep Time: </span>${recipe.prepTime}&nbsp&nbsp&nbsp&nbsp<span>CookTime: </span>${recipe.cookTime}</ul>
+              </div>`
+    }).join("&nbsp")
+  })
+    .catch((error) => {
+      console.error('Error:', error);
+    })
+};
 
 function getSpecials() {
   fetch("http://localhost:3001/specials")
@@ -90,7 +95,7 @@ function getSpecials() {
                     <p>Details: ${special.text}</p>
                   </div>
                 </div>`
-      })
+      }).join("&nbsp")
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -107,7 +112,6 @@ function getValues() {
   $('#ingredients').val().split(", ").map(function (i) {
     ingredients.push({ "name": i });
   });
-  console.log(ingredients)
   var directions = $("#directions").val();
   newData = {
     title: recipeName,
@@ -147,88 +151,3 @@ function success() {
     $(".success").removeClass("animSuccess")
   }, 2000);
 }
-
-function getRecipes() {
-  Promise.all([
-    fetch('http://localhost:3001/recipes'),
-    fetch('http://localhost:3001/specials')
-  ]).then(function (responses) {
-    // Get a JSON object from each of the responses
-    return Promise.all(responses.map(function (response) {
-      return response.json();
-    }));
-  }).then(function (data) {
-    // Log the data to the console
-    // You would do something with both sets of data here
-    recipes = data[0];
-    specials = data[1];
-    // console.log(specials[0].ingredientId)
-    // if (specials.map(function (special) {
-    //   special.ingredientId
-    // })
-    // &&
-    // recipes.map(function (recipe) {
-    //   recipe.ingredients.map(function (ingredient) {
-    //     ingredient.uuid
-    //   })
-    // }) === )
-    // console.log(recipes[0].ingredients[0].uuid)
-    // console.log(recipes[0].ingredients[0].uuid === specials[0].ingredientId)
-      recipeDiv.innerHTML = recipes.map(function (recipe) {
-        if (recipe.images) {
-          image = recipe.images.small
-        } else {
-          image = "/img/pancake_mountain--s.jpg";
-        }
-
-        // for (var i = 0; i < specials.length; i++) {
-        //   console.log(specials[i].ingredientId)
-        // }
-
-        // for (var i = 0; i < recipe.ingredients.length; i++) {
-        //   for (var i = 0; i < specials.length; i++){
-            
-        //   }
-        //   console.log(recipe.ingredients[i].uuid)
-        // }
-        
-        // if (specials.map(function (special) { special.ingredientId }) ===
-        //   recipe.ingredients.map(function (ingredient) { ingredient.uuid }) ) {
-        //     console.log("working!")
-        //   }
-        
-        
-        // recipe.ingredients.map(function (ingredient) {
-        //   console.log(ingredient.uuid)
-        // })
-
-        let ingredientContent = recipe.ingredients.map(function (ingredient) {
-          if (ingredient.uuid === )
-          return `<li class="list-group-item" data-key="${ingredient.uuid}">${ingredient.amount} ${ingredient.measurement} - ${ingredient.name}</li>`
-        }).join("\n")
-
-        let directionContent = recipe.directions.map(function (direction) {
-          return `<li class="list-group-item">${direction.instructions}</li>`
-        }).join("\n")
-
-        return `
-                <div class="card-header recipeCardHeader recipe" data="${recipe.title}">
-                    <h2 class="card-title">${recipe.title}</h2>
-                    <img class="card-title" src="http://localhost:3001/${image}" >
-                    <h2 class="card-title">Servings: ${recipe.servings}</h2>
-                </div>
-                <div class="card-body">
-                    <ul class="recipe-ul">Ingredients:
-                        ${ingredientContent}
-                    </ul>
-                    <ulclass="recipe-ul">Directions:
-                        ${directionContent}
-                    </ul>
-                    <ul class="recipe-ul text-center">Prep Time:${recipe.prepTime}&nbsp&nbsp&nbsp&nbspCook Time:${recipe.cookTime}</ul>
-                </div>`
-      })
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    })
-};
